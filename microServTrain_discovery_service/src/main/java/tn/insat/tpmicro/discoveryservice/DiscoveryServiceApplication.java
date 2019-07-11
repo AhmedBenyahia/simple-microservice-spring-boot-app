c67a79f88049b8c32b23325b348020360ccef037
Slayver Sayto <slaysayto@gmail.com>
2019-07-11 16:20:52 +0100

    init repo

---
+++ microServTrain_discovery_service/src/main/java/tn/insat/tpmicro/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,16 @@
+package tn.insat.tpmicro.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+}
